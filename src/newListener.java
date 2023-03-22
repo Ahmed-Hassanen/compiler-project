036ca34d3c5e7860abb780dc1c8d8578558b08e3
Ahmed Hassanen <ahmed180116@feng.bu.edu.eg>
2023-03-22 16:28:00 +0200

    read and write files and rewriter

--- src/newListener.java
+++ src/newListener.java
@@ -1,10 +1,18 @@
-public class newListener extends JavaParserBaseListener{
-    private int blockNumber = 1;
-        @Override public void enterBlock(JavaParser.BlockContext ctx) {
-            System.out.println("block "+blockNumber+"\n");
-            System.out.println(ctx.getText());
-            blockNumber++;
-        }
+import org.antlr.v4.runtime.TokenStreamRewriter;
 
+public class newListener extends JavaParserBaseListener {
+    TokenStreamRewriter rewriter;
+
+    public newListener(TokenStreamRewriter rewriter) {
+        this.rewriter = rewriter;
     }
 
+    private int blockNumber = 1;
+
+    @Override
+    public void enterBlock(JavaParser.BlockContext ctx) {
+        rewriter.insertAfter(ctx.getStart(), "//Block " + blockNumber+"\n");
+        blockNumber++;
+    }
+
+}
