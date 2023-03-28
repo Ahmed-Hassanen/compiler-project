1344cbeeb415b5c45773c5dd80cb351f1160bee1
Ahmed Ashraf <elshehry2000@gmail.com>
2023-03-28 02:01:37 +0200

    Mark_visited_blocks

--- src/newListener.java
+++ src/newListener.java
@@ -1,18 +1,24 @@
 import org.antlr.v4.runtime.TokenStreamRewriter;
 
+
 public class newListener extends JavaParserBaseListener {
     TokenStreamRewriter rewriter;
+    private int blockNumber = 1;
 
     public newListener(TokenStreamRewriter rewriter) {
         this.rewriter = rewriter;
+        this.blockNumber = 1;
     }
-
-    private int blockNumber = 1;
 
     @Override
     public void enterBlock(JavaParser.BlockContext ctx) {
-        rewriter.insertAfter(ctx.getStart(), "//Block " + blockNumber+"\n");
+
+                rewriter.insertAfter(ctx.getStart(),"\nFileWriter outpt"+blockNumber+" = new FileWriter(\"output.txt\", true);\n" +
+                        "        outpt"+blockNumber+".write(\"Block number \" +"+blockNumber +"+ \" is visited\\n\");\n" +
+                        "        outpt"+blockNumber+".close();\n");
+
         blockNumber++;
     }
 
 }
+
