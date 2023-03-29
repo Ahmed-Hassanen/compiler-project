8b0aa92e3564fd6ae8c482e866130e602430fc0a
Ahmed Ashraf <elshehry2000@gmail.com>
2023-03-29 15:30:59 +0200

    Updated_Listener

--- src/newListener.java
+++ src/newListener.java
@@ -1,35 +1,31 @@
 import org.antlr.v4.runtime.TokenStreamRewriter;
 
-
 public class newListener extends JavaParserBaseListener {
     TokenStreamRewriter rewriter;
     private int blockNumber = 1;
-    boolean z=false;
+
     public newListener(TokenStreamRewriter rewriter) {
         this.rewriter = rewriter;
-        this.blockNumber = 1;
     }
     @Override
-    public void enterTypeDeclaration(JavaParser.TypeDeclarationContext ctx) {
-        if(!z) {rewriter.insertBefore(ctx.getStart(),"import java.io.FileWriter;\n");
-            z=true;
-        }
+    public void enterCompilationUnit(JavaParser.CompilationUnitContext ctx) {
+        rewriter.insertBefore(ctx.getStart(),"import java.io.FileWriter;\n");
     }
     @Override
     public void enterBlock(JavaParser.BlockContext ctx) {
         if(blockNumber==1) {
-            rewriter.insertAfter(ctx.getStart(), "\n \t \tFileWriter outpt" + blockNumber + " = new FileWriter(\"output.txt\");\n" +
+            rewriter.insertAfter(ctx.getStart(),
+                "\n \t \tFileWriter outpt" + blockNumber + " = new FileWriter(\"output.txt\");\n" +
                     "        outpt" + blockNumber + ".write(\"Block number \" +" + blockNumber + "+ \" is visited\\n\");\n" +
                     "        outpt" + blockNumber + ".close();\n");
         }
         else{
-            rewriter.insertAfter(ctx.getStart(), "\n \t \tFileWriter outpt" + blockNumber + " = new FileWriter(\"output.txt\", true);\n" +
+            rewriter.insertAfter(ctx.getStart(),
+                "\n \t \tFileWriter outpt" + blockNumber + " = new FileWriter(\"output.txt\", true);\n" +
                     "        outpt" + blockNumber + ".write(\"Block number \" +" + blockNumber + "+ \" is visited\\n\");\n" +
                     "        outpt" + blockNumber + ".close();\n");
         }
             blockNumber++;
-
     }
-
 }
 
